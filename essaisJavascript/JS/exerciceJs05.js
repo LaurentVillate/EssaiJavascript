083c366600b9e5a91d90535701021bb5161be255
80010-99-11 <80010-99-11@exchange.ad.afpanet>
2020-11-10 16:52:13 +0100

    dossier tests javascript

---
+++ essaisJavascript/JS/exerciceJs05.js
@@ -0,0 +1,14 @@
+var nom = window.prompt("Entrez votre nom");
+var prenom = window.prompt("Entrez votre prénom");
+if ( window.confirm("Etes-vous un homme?") == true){
+    window.alert("Bonjour monsieur " + prenom +" "+ nom + " bienvenu sur notre site web");
+}
+else{
+    window.alert("bonjour madame " + prenom +" "+ nom + " bienvenu sur notre site web");
+} 
+
+
+
+
+
+
